f30dbcfbb9a32aa90b55125970b71458a7fd0fdd
yaoxinc <yaoxinc@yonyou.com>
2017-08-22 21:32:58 +0800

    初始化

---
+++ src/Slider.js
@@ -0,0 +1,7 @@
+import React, { Component, PropTypes } from 'react';
+const propTypes = {};
+const defaultProps = {};
+class Slider extends Component {render(){return(<h2>Welcome use tinper-bee</h2> )}};
+Slider.propTypes = propTypes;
+Slider.defaultProps = defaultProps;
+export default Slider;
