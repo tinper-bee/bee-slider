f8de891c006ade11d5e5467666a7fd9a18426f69
yaoxinc <yaoxinc@yonyou.com>
2017-08-28 10:23:54 +0800

    修改代码

--- demo/demolist/Demo8.js
+++ demo/demolist/Demo8.js
@@ -17,7 +17,7 @@ class Demo8 extends Component {
     const { value, dragging, index, ...restProps } = props;
     return (
       <Slider.Tooltip
-        prefixCls="rc-tooltip"
+        prefixCls="u-slider-tooltip"
         overlay={value}
         visible={dragging}
         placement="top"
